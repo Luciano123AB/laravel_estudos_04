ca720782b64e9ccc9c908b81ab22e896b8549306
Luciano Eduardo <luciano123ab@gmail.com>
2026-01-06 19:54:56 -0300

    004(ConectandoMultiplas_BaseDados)

--- config/database.php
+++ config/database.php
@@ -30,21 +30,12 @@ return [
     */
 
     'connections' => [
-
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
-        'MyConnection' => [
+        'mysql_users' => [
             'driver' => 'mysql',
             'url' => '',
             'host' => 'localhost',
             'port' => 3306,
-            'database' => 'laravel_mysql_database',
+            'database' => 'laravel_mysql_auth',
             'username' => 'root',
             'password' => '24032004ABCD123',
             'unix_socket' => env('DB_SOCKET', ''),
@@ -58,6 +49,27 @@ return [
                 Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+
+        'mysql_app' => [
+            'driver' => 'mysql',
+            'url' => '',
+            'host' => 'localhost',
+            'port' => 3306,
+            'database' => 'laravel_mysql_app',
+            'username' => 'root',
+            'password' => '24032004ABCD123',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         // 'mysql' => [
         //     'driver' => 'mysql',
         //     'url' => env('DB_URL'),
@@ -77,57 +89,6 @@ return [
         //         Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
         //     ]) : [],
         // ],
-
-        'mariadb' => [
-            'driver' => 'mariadb',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
-        ],
-
     ],
 
     /*
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,19 @@ Route::get("/sqlite", function() {
         die("Não foi possível conectar com a base de dados. Erro: " . $e->getMessage());
     }
 });
+
+Route::get("/mysql_test_to_database", function() {
+    try {
+        //Primeira base de dados:
+        DB::connection("mysql_users")->getPdo();
+        echo "Sucesso: " . DB::connection("mysql_users")->getDatabaseName();
+        echo "<br>";
+
+        //Segunda base de dados:
+        DB::connection("mysql_app")->getPdo();
+        echo "Sucesso: " . DB::connection("mysql_app")->getDatabaseName();
+        echo "<br>";
+    } catch (\Exception $e) {
+        die("ERRO: " . $e->getMessage());
+    }
+});
